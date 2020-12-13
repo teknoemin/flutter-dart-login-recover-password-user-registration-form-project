4bb766fdffc78da3277ca561c6dd40659cc0783c
teknoemin <teknoemin@gmail.com>
2020-12-13 20:38:42 +0300

    Add files via upload

---
+++ anaSayfa.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(anaSayfa());
+}
+
+class anaSayfa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("1"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("2"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("3"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("4"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("5"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("6"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("7"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("8"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("9"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("10"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("11"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("12"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("13"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("14"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("15"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("16"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("17"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("18"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("19"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    width: 101,
+                    height: 115,
+                    child: Text("20"),
+                    padding: EdgeInsets.only(right: 8.0, bottom: 5.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.white, width: 1),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text("192855073"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text("40 % 20 = 0"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Text("9 - 10 - 11"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ uyeOl.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(uyeOl());
+}
+
+class uyeOl extends StatefulWidget {
+  @override
+  _uyeOlState createState() => _uyeOlState();
+}
+
+class _uyeOlState extends State<uyeOl> {
+  String eposta;
+  String kadi;
+  String sifre;
+  String tsifre;
+  final _degeranahtar = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Form(
+          key: _degeranahtar,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "E-posta Adresiniz",
+                    labelStyle: TextStyle(color: Colors.green),
+                    border: OutlineInputBorder(),
+                  ),
+                  onSaved: (value) {
+                    eposta = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Kullanıcı Adınız",
+                    labelStyle: TextStyle(color: Colors.green),
+                    border: OutlineInputBorder(),
+                  ),
+                  onSaved: (value) {
+                    kadi = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Parolanız",
+                    labelStyle: TextStyle(color: Colors.green),
+                    border: OutlineInputBorder(),
+                  ),
+                  onSaved: (value) {
+                    sifre = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Tekrar Parolanız",
+                    labelStyle: TextStyle(color: Colors.green),
+                    border: OutlineInputBorder(),
+                  ),
+                  onSaved: (value) {
+                    tsifre = value;
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RaisedButton(
+                      child: Text("Vazgeç"),
+                      onPressed: () => Navigator.pop(context, "/main"),
+                    ),
+                    RaisedButton(
+                      child: Text("Kaydol"),
+                      onPressed: () {
+                        _degeranahtar.currentState.save();
+                        if (eposta.isEmpty ||
+                            kadi.isEmpty ||
+                            sifre.isEmpty ||
+                            tsifre.isEmpty) {
+                          debugPrint("Bilgi alanları boş bırakılamaz!");
+                        } else if (sifre != tsifre) {
+                          debugPrint(
+                              "Parola ve Parola Tekrarınız Birbiri İle Uyuşmuyor.");
+                        } else {
+                          debugPrint("E-posta Adresiniz: $eposta");
+                          debugPrint("Parolanız: $sifre");
+                          Navigator.pop(context, "/girisEkrani");
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
